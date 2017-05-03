35f8a107d46f5ff42caa1e73ba0cdb30f8b02b44
Tiseno <math.lindgren@gmail.com>
2017-05-03 16:50:57 +0200

    added a marker component

---
+++ qr/src/Marker/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'index.css'
+
+export default class Marker extends Component {
+  render() {
+    const text = this.props.text
+    return (
+      <div>
+        {text}
+      </div>
+    )
+  }
+}
