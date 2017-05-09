af7a07c4c4eede1514fa7eb026625e159093c335
Jesper Sandström <jespersandstrom@gmail.com>
2017-05-09 13:53:48 +0200

    State management (#3)
    * Model for the app
    
    * Unsubscribing fixed
    
    * Merged

--- qr/src/DetailPage/index.jsx
+++ qr/src/DetailPage/index.jsx
@@ -2,11 +2,29 @@ import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 import {Page} from 'react-onsenui'
 import './index.css'
-
+import model from '../model'
 import CustomToolbar from './../CustomToolbar'
 import Marker from './../Marker'
 
 export default class DetailPage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.notify = this.notify.bind(this)
+    this.model = model
+  }
+
+  componentDidMount() {
+    this.model.subscribe(this)
+  }
+
+  componentWillUnmount() {
+    this.model.unsubscribe(this)
+  }
+
+  notify(newState) {
+    this.setState(newState)
+  }
 
   render() {
     const center = { lat: 59.3446561, lng: 18.0555958 }
@@ -27,6 +45,7 @@ export default class DetailPage extends Component {
         </div>
         <div className="detail-content">
           Hello This is a test
+          <button onClick={() => this.model.leaveQueue(this.props.queue)}>Leave</button>
           <br/>
           Probably need some more info here?
         </div>
--- qr/src/MainPage/index.js
+++ qr/src/MainPage/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Toolbar, Page, Button, Row, Col} from 'react-onsenui'
+import {Toolbar, Page, Button, Row, Col, Icon} from 'react-onsenui'
 import './index.css'
+import model from '../model'
 
 import CustomToolbar from './../CustomToolbar'
 import DetailPage from './../DetailPage'
@@ -11,11 +12,34 @@ export default class MainPage extends React.Component {
   constructor(props) {
     super(props)
     this.pushPage = this.pushPage.bind(this)
+    this.notify = this.notify.bind(this)
+    this.toggleJoinQueue = this.toggleJoinQueue.bind(this)
+    this.model = model
   }
 
-  pushPage() {
-    console.log("pushing the page")
-    this.props.navigator.pushPage({component: () => <DetailPage queue='HM'/>})
+  componentDidMount() {
+    this.model.subscribe(this)
+  }
+
+  componentWillUnmount() {
+    this.model.unsubscribe(this)
+  }
+
+  notify(newState) {
+    this.setState(newState)
+  }
+
+  toggleJoinQueue(queue) {
+    console.log(queue)
+    if (queue.inQueue) {
+      this.model.leaveQueue(queue)
+    } else {
+      this.model.joinQueue(queue)
+    }
+  }
+
+  pushPage(queue) {
+    this.props.navigator.pushPage({component: () => <DetailPage queue={queue}/>})
   }
 
   renderToolbar() {
@@ -27,38 +51,57 @@ export default class MainPage extends React.Component {
   }
 
   render() {
+    const { queues } = this.model.getState()
+    const renderQueue = (queue) => (
+      <div key={queue.id}
+           className="list__blue">
+
+        <div className="left"
+             onClick={() => this.pushPage(queue)}>
+          <p className="nomargin"><b>{queue.id}</b></p>
+          <small>{queue.address}</small>
+        </div>
+
+        <div className="right">
+
+          <div className="center inlineBlock">
+            <Icon icon={queue.inQueue ? 'ion-close-circled' : 'ion-checkmark-circled'}
+                      className={ 'main__icon_size ' + (queue.inQueue ? 'text-red' : '')}>
+            </Icon>
+            <br />
+            <small onClick={() => this.toggleJoinQueue(queue)}>
+              { queue.inQueue
+                  ? "Leave"
+                  : "Join"
+              }
+            </small>
+          </div>
+
+          <div className="center inlineBlock">
+            <div className="main__circle">
+              <div className="main__circle_adjuster">
+                30
+              </div>
+            </div>
+            <small>Mins</small>
+          </div>
+        </div>
+
+        <div className="clearBoth"></div>
+
+      </div>
+    )
     return (
       <Page renderToolbar={() => <CustomToolbar/>}>
         <div className="page-content">
           <div className="custom__header">Nearby Queues</div>
-
-          <div className="list__blue" onClick={this.pushPage}>
-
-            <div className="left">
-              <p className="nomargin"><b>Försäkringskassan</b></p>
-              <small>Roslagsgatan 29</small>
-            </div>
-
-            <div className="right">
-
-              <div className="center inlineBlock">
-                <ons-icon icon="ion-checkmark-circled" className="main__icon_size"></ons-icon><br />
-                <small>JOIN</small>
-              </div>
-
-              <div className="center inlineBlock">
-                <div className="main__circle">
-                  <div className="main__circle_adjuster">
-                    30
-                  </div>
-                </div>
-                <small>Mins</small>
-              </div>
-            </div>
-
-            <div className="clearBoth"></div>
-
-          </div>
+          <Row className='custom__row-header'>
+            <Col width="40%">Store</Col>
+            <Col>Place</Col>
+            <Col class='center'>Address</Col>
+            <Col></Col>
+          </Row>
+          { queues.map(renderQueue) }
         </div>
       </Page>
     )
